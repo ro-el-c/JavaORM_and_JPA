80824b789ee7780d87613e55fa2ae8f3b89a39ea
ro-el-c <chung3286@naver.com>
2023-09-16 20:26:22 +0900

    feat: 회원 조회

--- src/main/java/helloJpa/JpaMain.java
+++ src/main/java/helloJpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,7 +22,7 @@ public class JpaMain {
             em.persist(member);
 
             // 회원 수정
-            Member findMember = em.find(Member.class, 1L);
+            Member findMember = em.find(Member.class, 1L); // 회원 조회
             System.out.println("findMember.getId() = " + findMember.getId());
             System.out.println("findMember.getName() = " + findMember.getName());
             findMember.setName("helloB");
@@ -29,6 +30,12 @@ public class JpaMain {
             // 회원 삭제
 //            Member findMember = em.find(Member.class, 1L);
             em.remove(findMember);
+
+            // 회원 조회 - JPQL
+            List<Member> findMembers = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member m : findMembers) {
+                System.out.println("m.getName() = " + m.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
