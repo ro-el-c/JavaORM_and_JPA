a5933897e5cc534e2bfb47e58ca629cb58032899
ro-el <alcxzp@gmail.com>
2023-09-20 17:38:00 +0900

    feat: 실전 예제 - 요구사항 분석과 기본 매핑

---
+++ src/main/java/jpashop/ShopMain.java
@@ -0,0 +1,26 @@
+package jpashop;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class ShopMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("helloJpa");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        
+        try {
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ src/main/java/jpashop/domain/Order.java
@@ -0,0 +1,19 @@
+package jpashop.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
