2568c020181989585209916fbb7f3a1c6f188a6c
ro-el <alcxzp@gmail.com>
2023-10-04 20:41:55 +0900

    feat: 일대일 양방향 연관관계

--- src/main/java/mapping_ex/Locker.java
+++ src/main/java/mapping_ex/Locker.java
@@ -1,14 +1,14 @@
 package mapping_ex;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+//@Entity
 public class Locker {
     @Id @GeneratedValue
     @Column(name = "locker_id")
     private Long id;
     private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
 }
