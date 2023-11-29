686ed94c444dde004c9ffca56269c65f63ce0692
ro-el-c <chung3286@naver.com>
2023-11-29 16:28:51 +0900

    feat: 프록시와 연관관계 관리 - 고아 객체 (삭제)

--- src/main/java/cascade_ex/CascadeMain.java
+++ src/main/java/cascade_ex/CascadeMain.java
@@ -24,6 +24,12 @@ public class CascadeMain {
 //            em.persist(child1);
 //            em.persist(child2);
 
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
             tx.commit();
         } catch (Exception e) {
             System.out.println("e.getMessage() = " + e.getMessage());
