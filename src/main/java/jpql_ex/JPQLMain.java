0771777552dc206243d0453d36c8cdc13e1736c3
ro-el <chung3286@naver.com>
2023-12-06 19:05:45 +0900

    feat: 객체지향 쿼리 언어 - Criteria
    자바 코드로 JPQL 작성 가능하지만, 너무 복잡하고 실용성 없음

--- src/main/java/jpql_ex/JPQLMain.java
+++ src/main/java/jpql_ex/JPQLMain.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class JPQLMain {
@@ -18,10 +19,17 @@ public class JPQLMain {
         tx.begin();
 
         try {
-            List<Member> memberList = em.createQuery(
+            List<Member> kimList = em.createQuery(
                     "select m from Member m where m.name like '%kim%'"
                     , Member.class
             ).getResultList();
+
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> kimListWithCriteriaQuery = em.createQuery(cq).getResultList();
 
             tx.commit();
         } catch (Exception e) {
