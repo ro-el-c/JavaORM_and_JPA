f21d81151e2bf439a1fee9e9d77530164c8d9961
ro-el <chung3286@naver.com>
2023-12-06 16:10:37 +0900

    feat: 객체지향 쿼리 언어 - JPQL 예시
    이름에 kim을 포함한 회원 조회

---
+++ src/main/java/jpql_ex/JPQLMain.java
@@ -0,0 +1,36 @@
+package jpql_ex;
+
+import jpashop.domain.member.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.List;
+
+public class JPQLMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("helloJpa");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> memberList = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'"
+                    , Member.class
+            ).getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
