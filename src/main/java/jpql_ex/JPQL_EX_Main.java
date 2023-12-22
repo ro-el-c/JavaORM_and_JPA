2e84b26b97b8c0f4f35d930ed54f6ba232c5193c
ro-el-c <chung3286@naver.com>
2023-12-22 21:28:41 +0900

    chore: JPQL 소개 실습용 Class 생성

---
+++ src/main/java/jpql_ex/JPQL_EX_Main.java
@@ -0,0 +1,44 @@
+package jpql_ex;
+
+import jpashop.domain.member.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class JPQL_EX_Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("helloJpa");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            List<Member> kimList = em.createQuery(
+                    "select m from Member m where m.name like '%kim%'"
+                    , Member.class
+            ).getResultList();
+
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Member> query = cb.createQuery(Member.class);
+
+            Root<Member> m = query.from(Member.class);
+            CriteriaQuery<Member> cq = query.select(m).where(cb.equal(m.get("name"), "kim"));
+            List<Member> kimListWithCriteriaQuery = em.createQuery(cq).getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println("e.getMessage() = " + e.getMessage());
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
